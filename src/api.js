14253cc7b2c9b69c58062d9c082a87709b8ef938
seunghun <seunghun.chan@gmail.com>
2018-06-28 01:08:45 +0900

    update api.js

--- src/api.js
+++ src/api.js
@@ -3,12 +3,17 @@ import { sign } from 'jsonwebtoken';
 import { encode as queryEncode } from 'querystring';
 
 const getAllAssets = (access_key, secret_key) => {
-  const payload = { access_key, nonce: (new Date).getTime() };
+  const payload = {
+    access_key,
+    nonce: (new Date).getTime()
+  };
   const token = sign(payload, secret_key);
   const options = {
     method: "GET",
     url: "https://api.upbit.com/v1/accounts",
-    headers: {Authorization: `Bearer ${token}`}
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
   };
 
   return rq(options);
@@ -20,7 +25,9 @@ const printAllAssets = input => {
 
 const getAvailableOrder = (access_key, secret_key) => {
   const market = 'KRW-EOS'
-  const query = queryEncode({market});
+  const query = queryEncode({
+    market
+  });
   const payload = {
     access_key,
     nonce: (new Date).getTime(),
@@ -30,7 +37,9 @@ const getAvailableOrder = (access_key, secret_key) => {
   const options = {
     method: "GET",
     url: "https://api.upbit.com/v1/orders/chance?" + query,
-    headers: {Authorization: `Bearer ${token}`}
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
   };
 
   return rq(options);
@@ -41,10 +50,18 @@ const getCurrentPrice = id => {
 };
 
 const howMuchEarnedAssets = async (access_key, secret_key, id) => {
+  const currency = id.split('-')[1] || '';
   const currentPrice = JSON.parse(await getCurrentPrice(id))[0].trade_price;
-  const myAssets = JSON.parse(await getAllAssets(access_key, secret_key))[1];
+  const myAssets = JSON.parse(
+    await getAllAssets(access_key, secret_key)
+  ).filter(v => v.currency === currency)[0] || {};
+  const balance = myAssets.balance;
+  const avgPrice = myAssets.avg_krw_buy_price;
+  const rate = ((currentPrice / avgPrice) * 100 - 100).toFixed(2);
+  const principal = (balance * avgPrice).toFixed(0);
+  const totalKrw = (balance * currentPrice).toFixed(0);
 
-  console.log(`currency: ${myAssets.currency}\n balance: ${myAssets.balance}, buy: ${myAssets.avg_krw_buy_price}, current: ${currentPrice} => ${((currentPrice / myAssets.avg_krw_buy_price) * 100 - 100).toFixed(2)} %`);
+  console.log(`currency: ${currency}\n balance: ${balance}, buy: ${avgPrice}, current: ${currentPrice} => ${rate} %\n KRW: ${principal} => ${totalKrw} WON`);
   if (myAssets.avg_krw_buy_price < currentPrice) {
     console.log('Will be rich!');
   } else if (myAssets.avg_krw_buy_price === currentPrice) {
@@ -54,4 +71,6 @@ const howMuchEarnedAssets = async (access_key, secret_key, id) => {
   }
 };
 
-export { howMuchEarnedAssets };
+export {
+  howMuchEarnedAssets
+};
